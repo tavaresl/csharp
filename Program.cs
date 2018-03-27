9d2260f07770d8636532bb7251f5049eded63df8
Lucas Tavares <lucastavaresdarocha@gmail.com>
2018-03-27 15:32:17 -0300

    update menu

--- Program.cs
+++ Program.cs
@@ -4,38 +4,60 @@ using Cases;
 
 namespace myAoo
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var argument = args.Any() ? args[0] : ReadArgumentFromPrompt();
-            Run(argument);
-        }
+      // var chapter = args.Any() ? args[0] : null;
+      // Run(chapter);
 
-        private static string ReadArgumentFromPrompt()
-        {
-            Console.WriteLine("Which case would you like to run?");
-            return Console.ReadLine();
-        }
-
-        private static void Run(string cmd)
-        {
-            switch (cmd.ToLower())
-            {
-                case "fibonacci":
-                case "-fib":
-                    new Fibonacci().Run();
-                    break;
-
-                case "list":
-                case "listexercises":
-                    new ListExercises().Run();
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid argument");
-                    break;
-            }
-        }
+      if (args.Any())
+        Run(args[0]);
+      else
+        RunFromPrompt();
     }
+
+    private static void RunFromPrompt()
+    {
+      Console.WriteLine("Which chapter would you like to run?");
+      Console.WriteLine(@"
+        0 - Fibonacci
+        1 - Arrays and Lists
+      ");
+
+      string command;
+      int val;
+
+      do
+      {
+        command = Console.ReadLine();
+      }
+      while(!int.TryParse(command, out val));
+
+      Run(command);
+    }
+
+    private static void Run(string cmd)
+    {
+      switch (cmd.ToLower())
+      {
+        case "fibonacci":
+        case "-fib":
+        case "0":
+          new Fibonacci().Run();
+          return;
+
+        case "arrayandlists":
+        case "-aal":
+        case "1":
+          new ArraysAndLists().Run();
+          return;
+
+        default:
+          Console.WriteLine("Invalid argument");
+          RunFromPrompt();
+          return;
+      }
+    }
+  }
 }
